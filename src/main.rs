423fd50352f79ddcca21dbe4f76dcc472c8416a2
Sandro Reis <consultorsandro@hotmail.com>
2025-05-12 08:35:05 -0300

    Class 124

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
+    //Class 124
+    let mut coffee = String::from("mocha");    
+    let a = &mut coffee;
+    println!("{}", a);
+    let b = a;
+    println!("{}", b);
+
+}
+/*
     // Class 122
     let car = String::from("red");
     let ref1 = &car;
     let ref2 = &car;
     println!("{} and {} and {}.", ref1, ref2, &car);
-
-}
+*/
 /*
 //class 121
 let mut current_meal = String::new();
