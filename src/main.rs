a4fd8e34512bd6f0313929b197e42385986a0f26
Sandro Reis <consultorsandro@hotmail.com>
2025-05-12 08:04:03 -0300

    Class 122

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
+    // Class 122
+    let car = String::from("red");
+    let ref1 = &car;
+    let ref2 = &car;
+    println!("{} and {} and {}.", ref1, ref2, &car);
+
+}
+/*
 //class 121
 let mut current_meal = String::new();
 
 add_flour(&mut current_meal);
 show_my_meal(&current_meal);
-
-}
+*/
+//Class 121 functions outside main
 //meal: String
 //mut meal : String
 //meal: &String
